f9d6fa9ee3d71d39226ef8191b31fbbae957fdc1
vaida898 <freelpfm@gmail.com>
2020-06-27 16:28:43 +0000

    updated files

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-
-const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
